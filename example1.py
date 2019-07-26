5ae311a6936a76ef633e21eafc1d6654e96fb48b
Garry Baker <trading@chillinet.com.au>
2019-07-25 19:46:29 -0700

    adding docs

---
+++ example1.py
@@ -0,0 +1,22 @@
+def test1(arg1, arg2):
+    """
+    A simple function takes 2 arguments and returns arg1.
+
+    :param arg1: A value
+
+    :param arg2: A Value
+
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
+
+def prod(x,y):
+    """
+    Function that multiplies 2 values
+    :param x: A value
+    :param y: A value
+    :return:  a, A Value
+    """
+    a=x*y
+    return a
